f5e19320af6b2184efd079a92352eb284154aa2b
Sutama Mitra <sutama.mitra@outlook.com>
2020-06-27 10:38:10 -0400

    Sutama Comment Added

--- ISM6225 In-class GitHub site/Program.cs
+++ ISM6225 In-class GitHub site/Program.cs
@@ -7,7 +7,7 @@ namespace ISM6225_In_class_GitHub_site
         static void Main(string[] args)
         {
             Console.WriteLine("Hello team! I am Simon.");
-
+            Console.WriteLine("Hello Simon! I am Sutama.");
             Console.ReadLine();
         }
     }
