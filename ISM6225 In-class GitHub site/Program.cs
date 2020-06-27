17081858dcbc602ff3f97981f75b893aae42afcb
Simon <sli8@usf.edu>
2020-06-27 00:22:57 -0400

    Add project files.

---
+++ ISM6225 In-class GitHub site/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ISM6225_In_class_GitHub_site
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello team! I am Simon.");
+
+            Console.ReadLine();
+        }
+    }
+}
